0846422df75452d284bcbd016429423e13e626d6
Akadet Mathurapoj <akadet.m@linecorp.com>
2020-06-14 19:28:29 +0700

    example custom type

---
+++ play_tsc/app/01_types/04_custom_type.ts
@@ -0,0 +1,87 @@
+type MyRect = {
+  width: number
+  height: number
+  area (): number
+}
+
+// typescript has type inference so we don't need to add keyword implement MyRect
+export class Rect {
+  width: number
+  height: number
+
+  area (): number {
+    return this.width * this.height
+  }
+}
+
+export function getArea (rect: MyRect): number {
+  return rect.area()
+}
+
+//  intersection type
+export type Object3D = {
+  depth: number
+}
+
+export type Cube = MyRect & Object3D
+
+//  constant type
+export type Complete = 'completed'
+export const xType: Complete = 'completed'
+
+//  union type
+type NullableNumber = number | null
+export const nullable1: NullableNumber = 10
+export const nullable2: NullableNumber = null
+
+export function plus (lhs: NullableNumber, rhs: NullableNumber): number {
+  if (!lhs || !rhs) {
+    return 0
+  }
+
+  return lhs + rhs // typescript can infer lhs and rhs type as number
+}
+
+type TaskStatus = 'complete' | 'pending'
+export const xTaskStatus: TaskStatus = 'pending'
+
+// type guard
+export function isString (input: string | number): input is string {
+  return typeof input === 'string'
+}
+
+export function processInput (input: string | number): void {
+  if (isString(input)) {
+    //  typescript can infer input type as string
+    console.log(input + input)
+  } else {
+    //  typescript can infer input type as number
+    console.log(input + input)
+  }
+}
+
+//  discriminated union
+interface Square {
+  kind: 'square'
+  size: number
+}
+
+interface Regtandle {
+  kind: 'regtandle'
+  width: number
+  height: number
+}
+
+interface Circle {
+  kind: 'circle'
+  radius: number
+}
+
+export type Shape = Square | Regtandle | Circle
+export function areaShape (shape: Shape): number {
+  switch (shape.kind) {
+    case 'square': return shape.size * shape.size
+    case 'regtandle': return shape.width * shape.height
+    case 'circle': return Math.PI * shape.radius ** 2
+  }
+}
--- play_tsc/app/my.ts
+++ play_tsc/app/my.ts
@@ -2,6 +2,7 @@ import 'source-map-support/register'
 import * as basic from './01_types/01_basictype'
 import * as cls from './01_types/02_class'
 import * as interf from './01_types/03_interface'
+import * as custom from './01_types/04_custom_type'
 
 // inport examples
 // import { Func1, Func2 } from './funcs'
@@ -24,3 +25,24 @@ console.log(reg.area())
 const reg2 = interf.SecondRect
 const secondReg = interf.area(reg2)
 console.log(secondReg)
+
+const reg3 = new custom.Rect()
+reg3.width = 3
+reg3.height = 4
+console.log(custom.getArea(reg3))
+
+const cube: custom.Cube = {
+  width: 2,
+  height: 3,
+  depth: 4,
+  area: (): number => 24
+}
+console.log(cube.area())
+console.log(custom.plus(1, 2))
+console.log(custom.plus(1, null))
+console.log(custom.xTaskStatus)
+console.log(custom.isString('123'))
+console.log(custom.isString(123))
+
+custom.processInput('456')
+custom.processInput(456)
